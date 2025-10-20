649e88e956a5251315353b6dc66ad8969d8a2180
jessica98d <jessicasdougherty7@gmail.com>
2025-10-20 18:06:14 -0400

    Update Reducer.cpp
    other version did not work with the other files as written

--- Reducer.cpp
+++ Reducer.cpp
@@ -3,23 +3,21 @@
 
 namespace mr {
 
-Reducer::Reducer(FileManager& fm, std::string_view outputDir)
-    : fileManager_(fm), outputDir_(outputDir),
-      successFilePath_(std::string(outputDir_) + "/_SUCCESS.txt") 
-{
+Reducer::Reducer(FileManager& fm, const std::string& outputDir)
+    : fileManager_(fm), outputDir_(outputDir) {
     fileManager_.ensureDir(outputDir_);
-    outFilePath_ = std::string(outputDir_) + "/word_counts.txt";
-    // Truncate previous output
+    outFilePath_ = outputDir_ + "/word_counts.txt";
+    // truncate previous output
     fileManager_.writeAll(outFilePath_, "");
 }
 
-void Reducer::reduce(std::string_view word, const std::vector<Count>& counts) {
-    const auto total = std::accumulate(counts.begin(), counts.end(), 0);
-    fileManager_.appendLine(outFilePath_, std::string(word) + "\t" + std::to_string(total));
+void Reducer::reduce(const Word& word, const std::vector<Count>& counts) {
+    int total = std::accumulate(counts.begin(), counts.end(), 0);
+    fileManager_.appendLine(outFilePath_, word + "\t" + std::to_string(total));
 }
 
 void Reducer::markSuccess() {
-    fileManager_.appendLine(successFilePath_, "ok");
+    fileManager_.appendLine(outputDir_ + "/_SUCCESS.txt", "ok");
 }
 
 } // namespace mr
